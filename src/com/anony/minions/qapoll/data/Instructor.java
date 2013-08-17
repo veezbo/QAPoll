69e91117592f7f2911c01762187a9c4483c89553
Saeid Farivar <saeid.a@sisa.samsung.com>
2013-08-17 14:39:28 -0700

    Addded some data classes.

---
+++ src/com/anony/minions/qapoll/data/Instructor.java
@@ -0,0 +1,11 @@
+package com.anony.minions.qapoll.data;
+
+public class Instructor extends User {
+
+	@Override
+	public boolean hasPermission(Permission permission) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/com/anony/minions/qapoll/data/Student.java
@@ -0,0 +1,11 @@
+package com.anony.minions.qapoll.data;
+
+public class Student extends User {
+
+	@Override
+	public boolean hasPermission(Permission permission) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
